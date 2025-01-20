892178b22e2493865d93dfb02fbf2d1fd9796c66
ComboBySlovakia <combobyslovakia@gmail.com>
2025-01-20 08:29:20 +0100

    Commit 1

---
+++ server/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const Ajv = require("ajv");
+
+const ajv = new(Ajv);
+
+const crypto = require("crypto");
+
+const app = express();
+
+const port = 8000;
+
+app.post("",)
